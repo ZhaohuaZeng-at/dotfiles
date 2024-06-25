f3451d62178bfcae99accff865bf17eae4cf44b6
Zhaohua Zeng <zhaohua.zeng@airtable.com>
2024-06-25 13:52:56 -0700

    Update to use Brewfile

---
+++ Brewfile
@@ -0,0 +1,14 @@
+cask_args appdir: '/Applications'
+
+tap 'homebrew/bundle'
+
+tap 'neovim/neovim' || true
+brew 'fzf'
+
+cask 'font-fira-code-nerd-font'
+
+brew 'thefuck'
+brew 'zoxide'
+brew 'eza'
+
+cask 'wezterm'
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,25 @@
-#!/bin/zsh
-set -eoxu pipefail
+#!/usr/bin/env bash
 
-brew install $(<brew-packages.txt)
+cd "$(dirname "$0")/.."
+DOTFILES_ROOT=$(pwd -P)
 
-# Create symbolic link for wezterm
-ln -s $HOME/.dotfiles/wezterm/.wezterm.lua $HOME/.wezterm.lua
+echo ''
+
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+setup_wezterm() {
+  # Create symbolic link for wezterm
+if [ -f $HOME/.wezterm.lua ]; then
+   info "$HOME/.wezterm.lua exists. Skipping wezterm setup."
+else
+   info "Configure wezterm.."
+  ln -s $HOME/.dotfiles/wezterm/.wezterm.lua $HOME/.wezterm.lua
+fi
+}
+
+info 'Install packages with brew'
+brew bundle
+
+setup_wezterm
